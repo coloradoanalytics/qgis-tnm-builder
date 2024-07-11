da2520c5eef6b46b897a79604e978198f28fd778
Joshua Leasure <joshua@coan.co>
2024-07-11 16:19:15 -0600

    create multiple receivers for multi-level locations

--- receivers_conversion.py
+++ receivers_conversion.py
@@ -16,41 +16,49 @@ def convert_receivers(root, receivers_layer):
 
     # Create a new receivers element
     receivers = ET.SubElement(root, "receivers")
-    for sequence_number, feature in enumerate(receivers_layer.getFeatures(), start=1):
-        receiver = ET.SubElement(receivers, "receiver")
-        ET.SubElement(receiver, "name").text = feature["name"]
-        ET.SubElement(receiver, "sequenceNumber").text = str(sequence_number)
-        ET.SubElement(receiver, "heightAboveTerrain").text = str(feature["height"])
-        ET.SubElement(receiver, "dwellingUnits").text = "1"
-        ET.SubElement(receiver, "defaultAdjustment").text = "0"
-        ET.SubElement(receiver, "noiseReductionGoal").text = "0"
-        ET.SubElement(receiver, "existingLEQ").text = "0"
-        ET.SubElement(receiver, "impactLEQ").text = "0"
-        ET.SubElement(receiver, "impactIncrease").text = "0"
-        ET.SubElement(receiver, "inactive").text = "false"
-        ET.SubElement(receiver, "roadAdjustmentSet")
-        
-        points = ET.SubElement(receiver, "points")
-        geom = feature.geometry()
-        if geom:
-            point = geom.asPoint()
-            pt = ET.SubElement(points, "point")
-            ET.SubElement(pt, "pointNumber").text = "0"
-            ET.SubElement(pt, "OrderingNumber").text = "0"
-            ET.SubElement(pt, "theX").text = str(point.x())
-            ET.SubElement(pt, "theY").text = str(point.y())
-            ET.SubElement(pt, "theZ").text = str(feature["elevation"])
+    sequence_number = 1
+    for feature in receivers_layer.getFeatures():
+        name_base = feature["name"]
+        base_height = feature["height"]
+        levels = feature["levels"]
+        height_per_level = feature["height per level"]
 
-        ET.SubElement(receiver, "ReceiverResults", attrib={f"{{{namespaces['xsi']}}}nil": "true"})
-        ET.SubElement(receiver, "receiverResultsForEquipment", attrib={f"{{{namespaces['xsi']}}}nil": "true"})
+        for level in range(1, levels + 1):
+            receiver = ET.SubElement(receivers, "receiver")
+            ET.SubElement(receiver, "name").text = f"{name_base}_level_{level}"
+            ET.SubElement(receiver, "sequenceNumber").text = str(sequence_number)
+            ET.SubElement(receiver, "heightAboveTerrain").text = str(base_height + (level - 1) * height_per_level)
+            ET.SubElement(receiver, "dwellingUnits").text = "1"
+            ET.SubElement(receiver, "defaultAdjustment").text = "0"
+            ET.SubElement(receiver, "noiseReductionGoal").text = "0"
+            ET.SubElement(receiver, "existingLEQ").text = "0"
+            ET.SubElement(receiver, "impactLEQ").text = "0"
+            ET.SubElement(receiver, "impactIncrease").text = "0"
+            ET.SubElement(receiver, "inactive").text = "false"
+            ET.SubElement(receiver, "roadAdjustmentSet")
+            
+            points = ET.SubElement(receiver, "points")
+            geom = feature.geometry()
+            if geom:
+                point = geom.asPoint()
+                pt = ET.SubElement(points, "point")
+                ET.SubElement(pt, "pointNumber").text = "0"
+                ET.SubElement(pt, "OrderingNumber").text = "0"
+                ET.SubElement(pt, "theX").text = str(point.x())
+                ET.SubElement(pt, "theY").text = str(point.y())
+                ET.SubElement(pt, "theZ").text = str(feature["elevation"])
+
+            ET.SubElement(receiver, "ReceiverResults", attrib={f"{{{namespaces['xsi']}}}nil": "true"})
+            ET.SubElement(receiver, "receiverResultsForEquipment", attrib={f"{{{namespaces['xsi']}}}nil": "true"})
+
+            sequence_number += 1
 
     return root
-
 
 # Example XML output:
 #   <receivers>
 #     <receiver>
-#       <name>Receiver-1</name>
+#       <name>Receiver-1_level_1</name>
 #       <sequenceNumber>1</sequenceNumber>
 #       <heightAboveTerrain>5</heightAboveTerrain>
 #       <dwellingUnits>1</dwellingUnits>
@@ -72,5 +80,11 @@ def convert_receivers(root, receivers_layer):
 #       </points>
 #       <ReceiverResults xsi:nil="true" />
 #       <receiverResultsForEquipment xsi:nil="true" />
+#     </receiver>
+#     <receiver>
+#       <name>Receiver-1_level_2</name>
+#       <sequenceNumber>2</sequenceNumber>
+#       <heightAboveTerrain>10</heightAboveTerrain>
+#       <!-- similar structure as above -->
 #     </receiver>
 #   </receivers>
